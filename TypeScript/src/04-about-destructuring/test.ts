c6e2aa66bb1e29653424a5476c68b8e4c13dfddd
RoberCastro <robertoicastro@gmail.com>
2017-03-14 23:10:05 +0100

    Modif test 4

--- TypeScript/src/04-about-destructuring/test.ts
+++ TypeScript/src/04-about-destructuring/test.ts
@@ -1,39 +1,40 @@
 import * as chai from 'chai';
 var expect = chai.expect;
 
-describe.only('about destructuring arrays', () => {
-  it.only('1-should extract value from array', () => {
+describe('about destructuring arrays', () => {
+  it('1-should extract value from array', () => {
     let [firstValue] = [1];
     expect(firstValue).to.equal(1);
   });
 
   it('2-should swap two variables, in one operation', () => {
+
     let [x, y] = ['ax', 'why'];
-    [x, y] = [x, y];
+    [x, y] = [y, x];
     expect([x, y]).to.eql(['why', 'ax']);
   });
 
   it('3-should count leading commas', () => {
     const all = ['ax', 'why', 'zet'];
-    const [,z] = all;
+    const [ a,  b, z] = all;
     expect(z).to.equal('zet');
   });
 
   it('4-should extract from nested arrays', () => {
     const user = [['Some', 'One'], 23];
-    const [firstName, surname, age] = user;
+    const [[firstName, surname], age] = user;
 
     expect(`${firstName} ${surname} = ${age} years`).to.equal('Some One = 23 years');
   });
 
   it('5-should chain assignments', () => {
     let c: number, d: number;
-    let a, b = [c, d] = [1, 2];
+    let [a, b] = [c, d] = [1, 2];
     expect([a, b, c, d]).to.eql([1, 2, 1, 2]);
   });
 
   it('6-should work in for-of loop', () => {
-    for (var [ a, b] of [[0, 1, 2]]) { }
+    for (var [ a, b] of [[0, 1, 2]]) {  }
     expect([a, b]).to.eql([1, 2]);
   });
 });
@@ -41,27 +42,28 @@ describe.only('about destructuring arrays', () => {
 describe('about destructuring objects', () => {
   it('1-should be simple', () => {
     const x = { x: 1 };
+    const {x} = x;
     expect(x).to.equal(1);
   });
 
   it('2-should find variables properties', () => {
     const magic = { first: 23, second: 42 };
-    const {magic: second} = { magic };
+    const { second} = magic;
     expect(second).to.equal(42);
   });
 
   it('3-should mix object and array', () => {
-    const {z: [x]} = { z: [23, 42] };
+    const {z: [y, x]} = { z: [23, 42] };
     expect(x).to.equal(42);
   });
 
   it('4-should mix array and object', () => {
-    const [, {lang}] = [null, [{ env: 'browser', lang: 'ES6' }]];
+    const [, [{lang}]] = [null, [{ env: 'browser', lang: 'ES6' }]];
     expect(lang).to.equal('ES6');
   });
 
   it('5-should missing refs become undefined ?', () => {
-    const {z} = { x: 1, z: 2 };
+    const {z} = { x: 1, z: 2 } == undefined;
     expect(z).to.equal(undefined);
   });
 
