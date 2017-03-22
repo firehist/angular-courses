6a587dbfa1843d0a8a70a22e0b1f10c501bddd08
Benjamin Longearet <firehist@gmail.com>
2017-03-22 08:35:54 +0100

    Fix typo in 06

--- Ressources/Challenges/06_More on components/src/app/app.module.ts
+++ Ressources/Challenges/06_More on components/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-import { ROUTES } from './app.route';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { ProductListComponent } from './+products/product-list.component';
 import { ArrayFilterPipe } from './shared/pipes/array-filter.pipe';
